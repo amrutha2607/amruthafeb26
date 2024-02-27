38f4a9595c8b754aab285ac976c30b5eae428668
amrutha2607 <153982436+amrutha2607@users.noreply.github.com>
2024-02-27 11:39:33 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,2 +1,7 @@
-print "good morning"
-
+class Demo{
+	public static void main(String [] args){
+		System.out.println("Hello All");
+		System.out.println("Hello Jenkins");
+		System.out.println("Hello Git");
+	}
+}
